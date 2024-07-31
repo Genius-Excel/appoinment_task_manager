b1d9bd9e0900ff62538593881b9a5cd3843f0f57
Genius-Excel <geniusexcel9@gmail.com>
2024-07-31 13:55:26 +0100

    This module contains utility helper functions.

---
+++ task/utilities.py
@@ -0,0 +1,24 @@
+import mailtrap as mt
+from django.conf import settings
+
+# Send email custom function.
+def custom_email_sender(email_address, subject, message, sender_name):
+    mail = mt.Mail(
+                    sender=mt.Address(email='noreply@easyappz.com', name=sender_name),
+                    to=[mt.Address(email=email_address)],
+                    subject=subject,
+                    text=message,
+                    html=None,
+                )
+
+    client = mt.MailtrapClient(token=settings.SMTP_API_TOKEN)
+    client.send(mail)
+
+
+email_address = 'geniusexcel9@gmail.com'
+email_subject = 'This custom function works.'
+email_message = 'Holla! This utility function works and can be used in oher modules.'
+sender_name = 'Favour Excel'
+
+
+custom_email_sender(email_address, email_subject, email_message, sender_name)
