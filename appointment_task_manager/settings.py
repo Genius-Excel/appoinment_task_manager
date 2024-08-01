f59cdebb08e03a721363dbdefa5fbacb00ccd5bd
Genius-Excel <geniusexcel9@gmail.com>
2024-08-01 14:05:07 +0100

    Settings module updated to previous version.

--- appointment_task_manager/settings.py
+++ appointment_task_manager/settings.py
@@ -30,8 +30,8 @@ DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # Application definition
 
