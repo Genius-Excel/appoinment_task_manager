bb9132602b973ebc4570cb673c1a9b3754350a6a
Genius-Excel <geniusexcel9@gmail.com>
2024-08-01 15:20:16 +0100

    Url path for 'say_hello' function included.

--- task/urls.py
+++ task/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.say_hello, name='say-hello'),
     path('api/create-appointment/', views.CreateClientAppointment.as_view()),
 
 ]
